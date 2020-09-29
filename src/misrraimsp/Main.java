361c56e05dc198dcb4eb4a4a32f94f3e27e3af4c
Misrraim <misrraimsp@gmail.com>
2020-09-29 11:00:12 +0100

    added aggregate rates functionality

--- src/misrraimsp/Main.java
+++ src/misrraimsp/Main.java
@@ -2,6 +2,7 @@ package misrraimsp;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -14,19 +15,46 @@ public class Main {
 	    }
 	    String path = args[0];
 	    int maxRate = Integer.parseInt(args[1]);
+        System.out.println("maxRate: " + maxRate);
+
+	    int[] rates = new int[1440]; // 24 * 60
 
         try {
             FileInputStream fis = new FileInputStream(path);
             Scanner sc = new Scanner(fis);
             while(sc.hasNextLine()) {
-                System.out.println(sc.nextLine());
+                String line = sc.nextLine();
+                System.out.println(line);
+                String[] parts = line.split(",");
+                int rate = Integer.parseInt(parts[1]);
+                System.out.println("Rate: " + rate);
+                int startIndex = mapToIndex(parts[2]);
+                System.out.println("Start Index: " + startIndex);
+                int endIndex = mapToIndex(parts[3]);
+                System.out.println("End Index: " + endIndex);
+                aggregate(startIndex, endIndex, rate, rates);
             }
             sc.close();
         }
         catch(FileNotFoundException e) {
             e.printStackTrace();
         }
+        Arrays.stream(rates).forEach(r -> System.out.println("Rate: " + r));
 
-        System.out.println("maxRate: " + maxRate);
+
+
+    }
+
+    private static void aggregate(int startIndex, int endIndex, int rate, int[] rates) {
+        int i = startIndex;
+        do {
+            i++;
+            rates[i%1440] += rate;
+        } while (i%1440 != endIndex);
+    }
+
+    private static int mapToIndex(String dayTime) {
+        String[] dayTimeParts = dayTime.split(":");
+        return (60 * Integer.parseInt(dayTimeParts[0])) + Integer.parseInt(dayTimeParts[1]);
     }
 }
