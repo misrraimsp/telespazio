c5f2b599c705ef14586ed98bdbfd74de5708fcee
Misrraim <misrraimsp@gmail.com>
2020-09-29 19:04:11 +0100

    added max load period detection functionality

--- src/misrraimsp/Main.java
+++ src/misrraimsp/Main.java
@@ -2,7 +2,9 @@ package misrraimsp;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -28,9 +30,9 @@ public class Main {
                 String[] parts = line.split(",");
                 int rate = Integer.parseInt(parts[1]);
                 System.out.println("Rate: " + rate);
-                int startIndex = mapToIndex(parts[2]);
+                int startIndex = mapTimeToIndex(parts[2]);
                 System.out.println("Start Index: " + startIndex);
-                int endIndex = mapToIndex(parts[3]);
+                int endIndex = mapTimeToIndex(parts[3]);
                 System.out.println("End Index: " + endIndex);
                 aggregate(startIndex, endIndex, rate, rates);
             }
@@ -41,7 +43,35 @@ public class Main {
         }
         Arrays.stream(rates).forEach(r -> System.out.println("Rate: " + r));
 
+        // obtain the max load periods
+        //
+        //
+        List<Integer[]> indexes = new ArrayList<>();
 
+        //initial setup: 0 -> 29
+        int currentLoad = 0;
+        for (int i = 0; i < 30; i++) {
+            currentLoad += rates[i];
+        }
+        int max = currentLoad;
+        indexes.add(new Integer[]{0, 29});
+        //rest: 30 -> 1439
+        for (int j = 30; j < 1440; j++) {
+            currentLoad += (rates[j] - rates[j - 30]);
+            if (max < currentLoad) {
+                max = currentLoad;
+                indexes.clear();
+                indexes.add(new Integer[]{(j - 29), j});
+            }
+            else if (max == currentLoad) {
+                indexes.add(new Integer[]{(j - 29), j});
+            }
+        }
+
+
+        for (int k = 0; k < indexes.size(); k++) {
+            System.out.println("Period " + k + ": {startIndex=" + indexes.get(k)[0] + "; endIndex=" + indexes.get(k)[1] + "}");
+        }
 
     }
 
@@ -53,7 +83,7 @@ public class Main {
         } while (i%1440 != endIndex);
     }
 
-    private static int mapToIndex(String dayTime) {
+    private static int mapTimeToIndex(String dayTime) {
         String[] dayTimeParts = dayTime.split(":");
         return (60 * Integer.parseInt(dayTimeParts[0])) + Integer.parseInt(dayTimeParts[1]);
     }
